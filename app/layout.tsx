8488be6a1be3ed0cd44a5981f88c52a829518aaa
gusdev07 <gustavo.gomes@eshows.com.br>
2026-05-19 13:52:31 -0300

    dsa

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,27 +79,21 @@ export default function RootLayout({
           defer
         />
 
-        {/* Meta Pixel */}
+        {/* UTMify Pixel */}
         <Script
-          id="meta-pixel"
+          id="utmify-pixel"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-              n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-              document,'script','https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1223791793200733');
-              fbq('track', 'PageView');
+              window.pixelId = "69fe2a66940a7faaa1ce8298";
+              var a = document.createElement("script");
+              a.setAttribute("async", "");
+              a.setAttribute("defer", "");
+              a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+              document.head.appendChild(a);
             `,
           }}
         />
-        <noscript>
-          <img height="1" width="1" style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1223791793200733&ev=PageView&noscript=1"
-          />
-        </noscript>
 
         {/* Google Analytics 4 (GA4) */}
         <Script
